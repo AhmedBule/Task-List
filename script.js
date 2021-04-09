0bfcc1ea36190903b3016426538c0eef53916b54
Ahmed Bule <ahmedbule01@gmail.com>
2021-04-09 21:48:45 +0300

    added a new function called addtasks

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Define UI Variables
 
-let form  = document.querySelector('#form')
+let form = document.querySelector('#form')
 let taskList = document.querySelector('.collection');
 let clearBtn = document.querySelector('.clear-tasks');
 let filter = document.querySelector('#filter');
 let taskInput = document.querySelector('#task');
 
 
+// Load all event listeners
 
+loadEventListeners();
+
+// Create a function called loadEventListeners
+
+function loadEventListeners() {
+    // Add Task Event
+    form.addEventListener('submit', addTask)
+
+}
+
+// Add Task
+
+function addTask(e) {
+
+
+    e.preventDefault();
+}
