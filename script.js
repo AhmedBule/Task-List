d265c50361f8b39e6356a911c30b26a081056207
Ahmed Bule <ahmedbule01@gmail.com>
2021-04-09 21:43:38 +0300

    defined the UI variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// Define UI Variables
+
+let form  = document.querySelector('#form')
+let taskList = document.querySelector('.collection');
+let clearBtn = document.querySelector('.clear-tasks');
+let filter = document.querySelector('#filter');
+let taskInput = document.querySelector('#task');
+
+
+
