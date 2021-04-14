d0e878774a7769e12edef98db6d810abcc048b33
Ahmed Bule <ahmedbule01@gmail.com>
2021-04-15 01:26:20 +0300

    played around with the localstorage

--- script.js
+++ script.js
@@ -82,6 +82,27 @@ function addTask(e) {
 }
 
 
+// Store Task in Local Storage Function 
+
+//(5) 
+
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+
+
+
 // Remove task
 
 //  (2) Second function
@@ -134,20 +155,3 @@ function filterTasks(e) {
 }
 
 
-// Store Task in Local Storage Function 
-
-//(5) 
-
-function storeTaskInLocalStorage(task) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    tasks.push(task);
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-}
